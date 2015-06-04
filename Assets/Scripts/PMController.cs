5cd2199a79a36d58210c93d36a1e206ec38f8266
toastrider <rzulkowski@gmail.com>
2015-06-04 15:56:25 -0400

    Menu Improvement
    Menu now opens when Party Members are clicked on.

---
+++ Assets/Scripts/PMController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class PMController : MonoBehaviour, IPointerClickHandler {
+	 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerClick (PointerEventData eventData){
+		//Debug.Log ("CLICKS!!!");
+
+		GameObject menu = GameObject.Find ("PMMenu");
+		menu.GetComponent<CanvasGroup> ().alpha = 1;
+		menu.GetComponent<CanvasGroup> ().interactable = true;
+	}
+}
